4e823421489b7861fbb89e9b4824720e6d55c798
dreamvrutik <vrutikhalani6@gmail.com>
2020-05-29 20:08:03 +0530

    Okay

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'textclean',         # How you named your package folder (MyLib)
-  packages = ['textclean'],   # Chose the same as "name"
+  name = 'textpreprocess',         # How you named your package folder (MyLib)
+  packages = ['textpreprocess'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Package to get Vocabulary , simple forms and vectors of sentences.',   # Give a short description about your library
   author = 'Vrutik Halani',                   # Type in your name
   author_email = 'vrutikhalani6@gmail.com',      # Type in your E-Mail
   url = 'https://dreamvrutik.github.io/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/dreamvrutik/textclean/archive/0.1.tar.gz',    # I explain this later on
-  keywords = ['textclean', 'remove punctuation', 'sentences to vectors','vocabulary'],   # Keywords that define your package best
+  download_url = 'https://github.com/dreamvrutik/textpreprocess/archive/0.1.tar.gz',    # I explain this later on
+  keywords = ['textpreprocess', 'remove punctuation', 'sentences to vectors','vocabulary'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'nltk',
-          'textclean',
+          'textpreprocess',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
